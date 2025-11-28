63cb084c64ffce00c580ccf1d607c17130b88ee3
ipvsami <itssamashouri@gmail.com>
2025-11-28 14:43:02 +0330

    ui problems fixed, snackbar changed to rtl, form fields done

--- lib/app/modules/auth/register/views/register_view.dart
+++ lib/app/modules/auth/register/views/register_view.dart
@@ -5,7 +5,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import '../../../../routes/app_pages.dart';
-import '../../widgets/form_tile.dart';
 import '../controllers/register_controller.dart';
 
 class RegisterView extends GetView<RegisterController> {
@@ -19,70 +18,190 @@ class RegisterView extends GetView<RegisterController> {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisSize: MainAxisSize.min,
             children: [
-              Padding(padding: EdgeInsets.only(top: 40.h, bottom: 32.h), child: Image.asset('assets/icons/lock_icon.png', height: 100.h, width: 100.w)),
+              Padding(
+                padding: EdgeInsets.only(top: 40.h, bottom: 32.h),
+                child: Image.asset(
+                  'assets/icons/lock_icon.png',
+                  height: 100.h,
+                  width: 100.w,
+                ),
+              ),
               Stack(
                 alignment: Alignment.topCenter,
                 children: [
-                  Container(width: Get.width, height: Get.height - 172.h, decoration: BoxDecoration(color: AppColors.third, borderRadius: BorderRadius.only(topLeft: Radius.circular(120.r)))),
+                  Container(
+                    width: Get.width,
+                    height: Get.height - 172.h,
+                    decoration: BoxDecoration(
+                      color: AppColors.third,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(120.r),
+                      ),
+                    ),
+                  ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Padding(padding: EdgeInsets.only(top: 40.h, bottom: 50.h), child: Text('ثبت نام', style: Theme.of(context).textTheme.titleLarge)),
                       Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-                        child: TextFormField(
-                          controller: controller.usernameController,
-                          textDirection: TextDirection.rtl,
-                          decoration: InputDecoration(labelText: 'نام کاربری', hintText: 'نام کاربری خود را وارد کنید', prefixIcon: Icon(Icons.person_2_outlined), border: OutlineInputBorder()),
+                        padding: EdgeInsets.only(top: 40.h, bottom: 50.h),
+                        child: Text(
+                          'ثبت نام',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16.w),
+                          child: Text(
+                            'نام کاربری',
+                            textAlign: TextAlign.right,
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
                         ),
                       ),
                       Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16.w,
+                          vertical: 16.h,
+                        ),
+                        child: TextFormField(
+                          controller: controller.usernameController,
+                          cursorColor: AppColors.primary,
+
+                          textDirection: TextDirection.rtl,
+                          decoration: InputDecoration(
+                            hintTextDirection: TextDirection.rtl,
+                            suffixIcon: Icon(
+                              Icons.person_2_outlined,
+                              color: AppColors.primary,
+                            ),
+                            hintText: 'نام کاربری خود را وارد کنید',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            left: 16.w,
+                            right: 16.w,
+                            top: 16.h,
+                          ),
+                          child: Text(
+                            'ایمیل',
+                            textAlign: TextAlign.right,
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16.w,
+                          vertical: 16.h,
+                        ),
                         child: TextFormField(
                           controller: controller.emailController,
                           textDirection: TextDirection.ltr,
+                          cursorColor: AppColors.primary,
+
                           keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(labelText: 'ایمیل', hintText: 'لطفا ایمیل خود را وارد کنید', prefixIcon: Icon(Icons.mail), border: OutlineInputBorder()),
+                          decoration: InputDecoration(
+                            hintTextDirection: TextDirection.rtl,
+                            suffixIcon: Icon(
+                              Icons.mail_outline,
+                              color: AppColors.primary,
+                            ),
+                            hintText: 'لطفا ایمیل خود را وارد کنید',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            left: 16.w,
+                            right: 16.w,
+                            top: 16.h,
+                          ),
+                          child: Text(
+                            'رمزعبور',
+                            textAlign: TextAlign.right,
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
                         ),
                       ),
                       Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16.w,
+                          vertical: 16.h,
+                        ),
                         child: TextFormField(
+                          cursorColor: AppColors.primary,
                           obscureText: true,
                           controller: controller.passwordController,
                           textDirection: TextDirection.ltr,
-                          decoration: InputDecoration(labelText: 'رمز عبور', hintText: 'رمز عبور خود را وارد کنید', prefixIcon: Icon(Icons.person_2_outlined), border: OutlineInputBorder()),
+                          decoration: InputDecoration(
+                            hintTextDirection: TextDirection.rtl,
+                            suffixIcon: Icon(
+                              Icons.lock_outline,
+                              color: AppColors.primary,
+                            ),
+                            hintText: 'رمز عبور خود را وارد کنید',
+                            border: OutlineInputBorder(),
+                          ),
                         ),
                       ),
                     ],
                   ),
                   Positioned(
-                    bottom: 32.h,
+                    bottom: 62.h,
                     left: 16.w,
                     right: 16.w,
                     child: Obx(
                       () => SizedBox(
                         width: Get.width,
                         child: ElevatedButton(
-                          onPressed: controller.isFormValid.value ? controller.register : null,
-                          child: controller.isLoading.value ? SizedBox(width: 24.w, height: 24.w, child: CircularProgressIndicator(color: Colors.white, strokeWidth: 2)) : Text('ثبت نام'),
+                          onPressed:
+                              controller.isFormValid.value
+                                  ? controller.register
+                                  : null,
+                          child:
+                              controller.isLoading.value
+                                  ? SizedBox(
+                                    width: 24.w,
+                                    height: 24.w,
+                                    child: CircularProgressIndicator(
+                                      color: Colors.white,
+                                      strokeWidth: 2,
+                                    ),
+                                  )
+                                  : Text('ثبت نام'),
                         ),
                       ),
                     ),
                   ),
                   SizedBox(height: 10.h),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          Get.toNamed(Routes.LOGIN);
-                        },
-                        child: Text('ثبت نام'),
-                      ),
-                      Text('حساب کاربری دارید؟', style: Theme.of(context).textTheme.bodyLarge),
-                    ],
+                  Positioned(
+                    bottom: 10.h,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Get.toNamed(Routes.LOGIN);
+                          },
+                          child: Text('ورود', style: TextStyle(height: 1)),
+                        ),
+                        Text(
+                          'حساب کاربری دارید؟',
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
--- lib/app/modules/cart/views/cart_view.dart
+++ lib/app/modules/cart/views/cart_view.dart
@@ -24,16 +24,7 @@ class CartView extends GetView<CartController> {
                 fontFamily: 'vazirmatn',
               ),
             ),
-            leading: IconButton(
-              onPressed: () {
-                Get.back();
-              },
-              icon: Icon(
-                Icons.arrow_back_ios_new,
-                color: AppColors.primary,
-                size: 30.sp,
-              ),
-            ),
+
             backgroundColor: Colors.transparent,
             elevation: 0,
             automaticallyImplyLeading: false,
@@ -92,7 +83,7 @@ class CartView extends GetView<CartController> {
                         child: TextField(
                           cursorColor: AppColors.text,
                           style: Theme.of(context).textTheme.bodyMedium,
-                          textDirection: TextDirection.rtl,
+                          textDirection: TextDirection.ltr,
                           decoration: InputDecoration(
                             hintText: 'کد تخفیف',
                             hintTextDirection: TextDirection.rtl,
@@ -149,7 +140,7 @@ class CartView extends GetView<CartController> {
                   padding: EdgeInsets.only(
                     left: 22.w,
                     right: 22.w,
-                    bottom: 140.h,
+                    bottom: 35.h,
                   ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -166,20 +157,21 @@ class CartView extends GetView<CartController> {
                     ],
                   ),
                 ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: 20.w,
+                    right: 20.w,
+                    bottom: 40.w,
+                  ),
+                  child: SizedBox(
+                    width: Get.width,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('تسویه حساب'),
+                    ),
+                  ),
+                ),
               ],
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 15.h),
-            child: SizedBox(
-              width: Get.width,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('تسویه حساب'),
-              ),
             ),
           ),
         ),
