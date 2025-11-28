5b0d4bf87641654aed74e326aa01828da7d244b2
ipvsami <itssamashouri@gmail.com>
2025-11-28 17:23:55 +0330

    home page leaeding buttons, search bar, search button done

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
+import 'package:coffee_store_app/app/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
@@ -9,42 +13,141 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('HomeView'), centerTitle: true),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              child: Text('ثبت نام'),
-              onPressed: () {
-                Get.toNamed('/register');
-              },
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 22.w, vertical: 28.h),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: AppColors.third,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.notifications,
+                      color: AppColors.primary,
+                      size: 30.sp,
+                    ),
+                  ),
+                ),
+                InkResponse(
+                  onTap: () {},
+                  child: Image.asset('assets/icons/menu.png'),
+                ),
+              ],
             ),
-            ElevatedButton(
-              child: Text('ورود'),
-              onPressed: () {
-                Get.toNamed('/login');
-              },
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 20.w),
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'قهوه خانه',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  Opacity(
+                    opacity: 0.7,
+                    child: Text(
+                      'بهترین قهوه برای شما',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            ElevatedButton(
-              child: Text('محصول'),
-              onPressed: () {
-                Get.toNamed('/product');
-              },
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20.h),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    cursorColor: AppColors.secondary,
+                    textDirection: TextDirection.rtl,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                    decoration: InputDecoration(
+                      hintTextDirection: TextDirection.rtl,
+                      hintText: 'جستجوی محصول',
+                      filled: true,
+                      fillColor: AppColors.buttonText.withAlpha(160),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: AppColors.black.withAlpha(60),
+                          width: 1.8.w,
+                        ),
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(12.r),
+                          bottomRight: Radius.circular(12.r),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: AppColors.black.withAlpha(60),
+                          width: 1.8.w,
+                        ),
+
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(12.r),
+                          bottomRight: Radius.circular(12.r),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: AppColors.black.withAlpha(60),
+                          width: 1.8.w,
+                        ),
+
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(12.r),
+                          bottomRight: Radius.circular(12.r),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15.w),
+                SizedBox(
+                  height: 52.3.h,
+                  width: 80.w,
+                  child: InkResponse(
+                    onTap: () {},
+                    child: Card(
+                      margin: EdgeInsets.zero,
+                      elevation: 0,
+                      color: AppColors.secondary,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          width: 1.5.h,
+                          color: AppColors.secondary,
+                        ),
+                        borderRadius: BorderRadiusGeometry.only(
+                          topLeft: Radius.circular(10.r),
+                          bottomLeft: Radius.circular(10.r),
+                        ),
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 20.w),
+                          child: Icon(
+                            Icons.search,
+                            color: AppColors.background,
+                            size: 28.sp,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            ElevatedButton(
-              child: Text('پروفایل'),
-              onPressed: () {
-                Get.toNamed('/profile');
-              },
-            ),
-            ElevatedButton(
-              child: Text('سبد خرید'),
-              onPressed: () {
-                Get.toNamed('/cart');
-              },
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
